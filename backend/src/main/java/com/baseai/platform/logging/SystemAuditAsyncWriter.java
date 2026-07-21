389e97208c86acb3eb70c610158aeb41709a0885
luohuatingyu <3524570351@qq.com>
2026-07-21 09:21:13 +0800

    Remove unused audit import

--- backend/src/main/java/com/baseai/platform/logging/SystemAuditAsyncWriter.java
+++ backend/src/main/java/com/baseai/platform/logging/SystemAuditAsyncWriter.java
@@ -1,6 +1,5 @@
 package com.baseai.platform.logging;
 
-import com.baseai.platform.domain.LoginLog;
 import com.baseai.platform.domain.OperationLog;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
