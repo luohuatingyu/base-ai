78e6aeb66e7e0c4f052dbe9a83a75a79ed54723b
luohuatingyu <3524570351@qq.com>
2026-07-22 10:34:19 +0800

    Limit sidebar icon imports

--- frontend/src/components/MenuNode.vue
+++ frontend/src/components/MenuNode.vue
@@ -14,12 +14,16 @@
 
 <script setup>
 import { computed } from 'vue'
-import * as ElementPlusIcons from '@element-plus/icons-vue'
+import {
+  Avatar, Briefcase, ChatDotRound, Collection, Connection, Cpu, Document, Guide, Link, List,
+  MagicStick, Menu as MenuIcon, OfficeBuilding, Operation, Promotion, Setting, Tickets, Tools, User
+} from '@element-plus/icons-vue'
 
 defineOptions({ name: 'MenuNode' })
 const props = defineProps({ item: { type: Object, required: true } })
 const children = computed(() => props.item.children || [])
+const iconMap = { Avatar, Briefcase, ChatDotRound, Collection, Connection, Cpu, Document, Guide, Link, List, MagicStick, Menu: MenuIcon, OfficeBuilding, Operation, Promotion, Setting, Tickets, Tools, User }
 
 /** 优先展示菜单配置图标；配置缺失或错误时回退为通用菜单图标。 */
-const menuIcon = computed(() => ElementPlusIcons[props.item.icon] || ElementPlusIcons.Menu)
+const menuIcon = computed(() => iconMap[props.item.icon] || MenuIcon)
 </script>
